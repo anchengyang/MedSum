e7682826729972141233ca4422ae7bfb4b4e67bf
MilkCatt <marcuschaoy@gmail.com>
2024-05-11 18:40:39 -0700

    Updated FilteringClient to include PICO and RQ Inputs

--- Classes/FilteringClient.py
+++ Classes/FilteringClient.py
@@ -10,6 +10,9 @@ from langchain.retrievers.self_query.base import SelfQueryRetriever
 from langchain.chains import RetrievalQA
 from langchain.prompts import PromptTemplate, SystemMessagePromptTemplate, HumanMessagePromptTemplate,  ChatPromptTemplate
 from IPython.display import display, Markdown
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_text_splitters import CharacterTextSplitter
 
 from dataclasses import dataclass
 
@@ -20,10 +23,9 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 class FilteringClient:
     def __init__(self, bg_data: list[Document]):
         self.bg_data = bg_data
-        self.data_length = len(bg_data)
         self.embeddings = OpenAIEmbeddings()
-        self.llm = ChatOpenAI(temperature=0)
-        # self.llm = ChatOpenAI(model_name="gpt-4", temperature=0, openai_api_key=OPENAI_API_KEY)
+        # self.llm = ChatOpenAI(temperature=0)
+        self.llm = ChatOpenAI(model_name="gpt-4", temperature=0.5, openai_api_key=OPENAI_API_KEY)
         self.persist_directory = "chroma"
         self.db = None
 
@@ -31,7 +33,7 @@ class FilteringClient:
         '''
         Splits document data into Langchain Chunks, returning a chunked document
         '''
-        text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
+        text_splitter = CharacterTextSplitter(chunk_size=500, chunk_overlap=0)
         chunks = text_splitter.split_documents(self.bg_data)
         return chunks
 
@@ -67,6 +69,11 @@ class FilteringClient:
                 type="integer",
             ),
             AttributeInfo(
+                name="Title",
+                description="The title of the paper", 
+                type="integer",
+            ),
+            AttributeInfo(
                 name="Abstract",
                 description="A concise summary of the research paper", 
                 type="string or list[string]"
@@ -83,40 +90,56 @@ class FilteringClient:
             enable_limit = True,
             verbose=True)
         return retriever
-    
-    def qa_func(self, system_template):
-        '''
-        Sets a prompt with human + system template.
-        Returns a RetrievalQA Object:
-        - Retriever retrieves relevant documents from the vector DB
-        '''
-        messages = [
-            SystemMessagePromptTemplate.from_template(system_template),
-            HumanMessagePromptTemplate.from_template("{question}")
-        ]
-        qa_prompt = ChatPromptTemplate.from_messages(messages)
-        #memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
-        #qa = ConversationalRetrievalChain.from_llm(llm=llm, retriever=vectorstore.as_retriever(), return_source_documents=False,combine_docs_chain_kwargs={"prompt": qa_prompt},memory=memory,verbose=True)
-        qa = RetrievalQA.from_chain_type(
-            llm=self.llm, 
-            retriever=self.get_retriever(), 
-            chain_type_kwargs={"prompt": qa_prompt},
-            verbose=True, 
-            return_source_documents=True)
 
-        return qa
     
-    def print_result(self, query, system_template):
-        '''
-        gets RetrievalQA object and calls it with a query.
-        Returns the query, results, and reference links/
-        '''
-        qa = self.qa_func(system_template)
-        result = qa(query)
+    def rag_result(self, research_question, no_of_articles, population, intervention, comparison, outcome):
+        prompt = PromptTemplate(
+        input_variables=["research_question", "population", "intervention", "comparison", "outcome", "no_of_articles"],
+        template=
+        """
+            You are a researcher performing a literature review based on a research question, PICO, and a number of documents to select.
+            PICO is an acronym for Population, Intervention, Comparison, and Outcome, and it's a framework used in research to structure questions and facilitate literature reviews.
+            Research Question: {input}
+            Population: {population}
+            Intervention: {intervention}
+            Comparison: {comparison}
+            Outcome: {outcome}
+            Number of Articles to Select: {no_of_articles}
+
+            The dataset provided is a pre-processed dataset of PubMed articles.
+            It includes the following columns:
+            'PMID': the PubMed ID of the published article,
+            'Title': the title of the published article,
+            'Abstract': an abstract of the published article,
+            ----------------
+            {context}
+            --------------
+
+            Based on these details, you are required to select {no_of_articles} articles that are relevant to the research question.
+            For each of these articles, provide:
+            - The title of the article
+            - The PubMed ID of the article
+            - A brief explanation why you selected it, including a snippet from the article that supports its relevance.
+
+
+            If there are actual numbers and percentages, state it.
+            """
+        )
+
+        question_answer_chain = create_stuff_documents_chain(
+            self.llm, prompt)
+        chain = create_retrieval_chain(self.get_retriever(), question_answer_chain)
+
+        result = chain.invoke({"input": research_question, 
+                            "no_of_articles": no_of_articles, 
+                            "population": population, 
+                            "intervention": intervention, 
+                            "comparison": comparison, 
+                            "outcome": outcome})       
 
         reference_links = []
         pmids = []
-        for doc in result["source_documents"]:
+        for doc in result["context"]:
             pmid = doc.metadata["PMID"]
             if pmid not in pmids:
                 pmids.append(pmid)
@@ -127,10 +150,10 @@ class FilteringClient:
 
         output_text = f"""
         ### Question: 
-        {result["query"]}
+        {result["input"]}
 
         ### Response: 
-        {result["result"]}
+        {result["answer"]}
 
         ### Reference Links:
         {formatted_reference_links}
@@ -138,5 +161,4 @@ class FilteringClient:
 
         print(Markdown(output_text).data)
         return result
-    
 
